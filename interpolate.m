aabc2a65bb54f9000fb42ace0642bf2b8434a760
Hovhannes Khachikyan <hovo.khc@gmail.com>
2018-04-30 22:14:00 -0400

    Added description & usage for interpolate function

--- interpolate.m
+++ interpolate.m
@@ -1,3 +1,12 @@
+%{
+ This function performs Interpolation by zero-padding in the frequancy
+ domain; uses FFT and IFFT.
+
+ Example: x = [1,2,3,4]
+          n = 2;
+          interpolate(x, n) will return 
+          [1, 1291/1189, 2, 5/2, 3, 1597/408, 4, 5/2]
+%}
 function out = interpolate(x, n)
     % represent x as column-vector
     x = x(:);
