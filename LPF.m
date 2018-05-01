68d61a18a2bf9c3a76af78287547052c547fbb68
Hovhannes Khachikyan <hovo.khc@gmail.com>
2018-04-30 22:14:37 -0400

    Added description low pass filter function

--- LPF.m
+++ LPF.m
@@ -1,12 +1,20 @@
+%{
+First Order Low Pass FIR Filter
+Source: http://sip.cua.edu/res/docs/courses/ee515/chapter04/ch4-5.pdf 
+%}
 function out = LPF(M)
     w = 0:0.001:pi;
-    z=exp(1i*w);
-    
+    % Definition of z: X(w) = X(z) | evaluated at z=e^jw
+    z = exp((1j)*w);
+  
+    % Normalized Cutoff frequency
     w_c = (pi/M)/pi;
     
     p = [cos(w_c),-2,cos(w_c)];
     r = roots(p);
     a = r(2);
+    
+    % Transfer function for the FIR Filter
     H_lp = ((1 - a)/2).*((1 + z.^-1)./(1 - a*(z.^-1)));
     
     out = real(ifft(H_lp));
