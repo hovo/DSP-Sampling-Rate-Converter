8460f0d07feb0a7794c484034ca2eaf155522282
Hovhannes Khachikyan <hovo.khc@gmail.com>
2018-04-30 21:17:57 -0400

    Delete upsamp.m

--- upsamp.m
+++
@@ -1,9 +0,0 @@
-% increases the sample rate of x by inserting n ? 1 zeros between samples.
-function y = upsamp(x, n)
-    j = 1;
-    for i=1:length(x)
-        y(j) = x(i);
-        y = [y zeros(1, n-1)];
-        j = j + n;
-    end
-end
