beb59ad194aeccb3ec2ea40ac78a6d9d17ddd38e
Hovhannes Khachikyan <hovo.khc@gmail.com>
2018-04-30 22:16:29 -0400

    Added description & usage for rate converter function

--- rateConverterLogic.m
+++ rateConverterLogic.m
@@ -1,21 +1,22 @@
-function rateConverterLogic(x, L, M, desired)
-    % 
-    % % Read the audio signal
-    % [x, Fs] = audioread('DSP.wav');
-
-    % % Temp Variables
-    % L = 320;
-    % M = 441;
-    % desired = 32000;
-
-    %{
-      SAMPLING RATE CONVERTER: BLOCK DIAGRAM
+%{
+ 
+   SAMPLING RATE CONVERTER: BLOCK DIAGRAM
 
            |----------------|     |-----|     |-----------------|
     ------>|  upsample by L |---->| LPF |---->| downsample by M |----->
            |________________|     |_____|     |_________________|
-    %}
-
+ 
+ This function performs sampling rate conversion
+ x: audio signal E.g. output from audioread
+ L: Integer factor for upsampling E.g 320
+ M: Integer factor for downsampling E.g 441
+ desiredFreq: Frequency we are trying to achive E.g. 32000 Hz 
+ 
+ Example: resampling an audio file from 44100 Hz to 32000 Hz
+          L/M = 32000/44100 = 320/441
+          desiredFreq = 32000
+%}
+function rateConverterLogic(x, L, M, desiredFreq)
     % Upsample the origianl audio file by a factor of L
     upsampled_x = myUpsample(x, L);
 
@@ -28,9 +29,9 @@ function rateConverterLogic(x, L, M, desired)
     % Downsample the interpolated signal by a factor of M
     downsampled_x = myDownsample(interpolated_x, M);
 
-    % Accomadte for power loss
+    % Accomadte for power loss: Multiply by constant gain factor of 100
     downsampled_x = downsampled_x.*100;
 
     % % Play the audio at desired frequency
-    sound(downsampled_x, desired);
+    sound(downsampled_x, desiredFreq);
 end
