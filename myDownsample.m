1e46c27fabef39f0c1989ae475da097efb720436
Hovhannes Khachikyan <hovo.khc@gmail.com>
2018-04-30 22:15:21 -0400

    Added description & usage for down-sampling function

--- myDownsample.m
+++ myDownsample.m
@@ -1,5 +1,11 @@
-% decreases the sample rate of x by keeping the first sample and then 
-% every nth sample after the first. 
+%{
+ This function decreases the sampling rate of x by keeping the first sample
+ and then every nth sample after the first.
+ 
+ Example: x = [1,2,3,4];
+          n = 2;
+          myDownsample(x, n) will return [1, 3]
+%}
 function y = myDownsample(x, n)
     y = x(1:n:length(x));
 end
