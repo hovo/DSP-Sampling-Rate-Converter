c715ac820d766053dfdf951946e54516df13322c
Hovhannes Khachikyan <hovo.khc@gmail.com>
2018-04-30 22:15:46 -0400

    Added description & usage for up-sampling function

--- myUpsample.m
+++ myUpsample.m
@@ -1,4 +1,11 @@
-% increases the sample rate of x by inserting n ? 1 zeros between samples.
+%{
+ This function increases the sampling rate of x by inserting n-1 zeros
+ between each sample.
+
+ Example: x = [1,2,3,4];
+          n = 2;
+          myUpsample(x,2) will return: [1,0,2,0,3,0,4]
+%}
 function y = myUpsample(x, n)
     j = 1;
     for i=1:length(x)
